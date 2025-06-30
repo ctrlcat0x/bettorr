f1fbad73a9a1e3290c5a3284a031b7f2f4101adc
ctrlcat0x <developer.sahilrana@gmail.com>
2025-06-30 23:24:50 +0530

    Add theme toggle functionality to enhance user experience. Implemented theme switching in script.js, updated index.html with a toggle button, and added corresponding CSS styles for new themes. Improved overall design consistency and interactivity.

--- script.js
+++ script.js
@@ -300,3 +300,23 @@ class BettorApp {
 document.addEventListener('DOMContentLoaded', () => {
     new BettorApp();
 }); 
+
+const toggleThemeBtn = document.querySelector('[data-theme-btn]');
+const themes = [' green ',' purple ',  ' seaGreen ', ' blue '];
+const storedTheme = localStorage.getItem('theme') || (localStorage.setItem('theme', themes[0]), themes[0]);
+function setTheme(theme) {
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+}
+function setThemeCycle() {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  setTheme(themes[(themes.indexOf(currentTheme) + 1) % themes.length]);
+}
+document.addEventListener('DOMContentLoaded', () => setTheme(storedTheme));
+toggleThemeBtn.addEventListener('click', setThemeCycle);
+document.addEventListener('keydown', event => {
+    if (event.code === ' add key here ') {
+      setThemeCycle();
+      event.preventDefault();
+    }
+});
